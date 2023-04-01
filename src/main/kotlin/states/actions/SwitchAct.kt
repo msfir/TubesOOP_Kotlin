e10b37ea38de76fd5e7d585d8793270a1d8e886d
msfir <ms.firdaus777@gmail.com>
2023-04-01 23:20:05 +0700

    Switch ability action

---
+++ src/main/kotlin/states/actions/SwitchAct.kt
@@ -0,0 +1,35 @@
+package tubes.oop.states.actions
+
+import tubes.oop.GameManager
+import tubes.oop.GameState
+import tubes.oop.entities.abilities.Switch
+import tubes.oop.extensions.ansi
+import tubes.oop.globals.ANSI_NAME
+import tubes.oop.globals.ANSI_SPECIAL_TEXT
+import tubes.oop.globals.inputValidate
+import tubes.oop.states.Action
+
+class SwitchAct : Action() {
+    override fun updateState(manager: GameManager): GameState {
+        val player = manager.currentPlayer
+        val ability = player.ability
+        if (ability !is Switch) {
+            println("Kamu tidak punya ability ${"SWITCH".ansi(*ANSI_SPECIAL_TEXT)}!")
+            return manager.stateRegistry.getState("player command")
+        }
+        println("${player.name.ansi(*ANSI_NAME)} menggunakan ability ${"SWITCH".ansi(*ANSI_SPECIAL_TEXT)}!");
+        println("Kartumu sekarang adalah ${player.leftCard} dan ${player.rightCard}")
+        val players = manager.players.filter { it.name != player.name }.toList()
+        println("Silahkan pilih pemain yang kartunya ingin anda tukar:")
+        players.withIndex().forEach {
+            println("\t${it.index + 1}. ${it.value.name.ansi(*ANSI_NAME)}")
+        }
+        val targetIdx = inputValidate(1, 6)
+        val target = players[targetIdx - 1]
+        ability.withAdditionalArgs(mapOf(Switch.Args.TARGET to target)).use()
+        println("Kedua kartu ${player.name.ansi(*ANSI_NAME)} telah ditukar dengan ${target.name.ansi(*ANSI_NAME)}")
+        println("Kartumu sekarang adalah ${player.leftCard} dan ${player.rightCard}")
+
+        return manager.stateRegistry.getState("next")
+    }
+}
