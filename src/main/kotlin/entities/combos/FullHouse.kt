dfcbeb0f20acca393a94701ef0145328dbafed61
msfir <ms.firdaus777@gmail.com>
2023-04-01 21:09:27 +0700

    Add Combo: Full House

---
+++ src/main/kotlin/entities/combos/FullHouse.kt
@@ -0,0 +1,52 @@
+package tubes.oop.entities.combos
+
+import tubes.oop.entities.Card
+import tubes.oop.entities.Combo
+import kotlin.Pair
+
+class FullHouse : Combo("Full House") {
+    override fun isThereCombo(playerCards: Pair<Card, Card>, tableCards: List<Card>): Boolean {
+        assert(tableCards.count() == 5)
+        cardList.clear()
+        val allCards = mutableListOf<Card>()
+        with(allCards) {
+            addAll(playerCards.toList())
+            addAll(tableCards)
+            sortByDescending { it.value() }
+        }
+        var foundPair = false
+        var foundTriplet = false
+        val temp1 = mutableListOf<Card>()
+        val temp2 = mutableListOf<Card>()
+        var i = 0
+        while (i < 5) {
+            if (allCards[i].number == allCards[i].number
+                && allCards[i + 1].number == allCards[i + 2].number
+            ) {
+                temp1.addAll(allCards.drop(i).take(3))
+                foundTriplet = true
+                i += 2
+            } else if (allCards[i].number == allCards[i + 1].number) {
+                temp2.addAll(allCards.drop(i).take(2))
+                foundPair = true
+                i++
+            }
+            if (foundTriplet && foundPair) {
+                val temp = temp1.toMutableList().also { it.addAll(temp2) }
+                if (temp.contains(playerCards.first) || temp.contains(playerCards.second)) {
+                    cardList.addAll(temp)
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    override fun clone(): Combo {
+        val newObject = FullHouse()
+        newObject.cardList.addAll(this.cardList)
+        return newObject
+    }
+
+    override fun value(): Double = 248.8881 + cardList.first().value() // max value: 261.9781
+}
